416f1f3431ac5bced5107b9d683193dd76c5e7f5
Terje Urnes <terje@urnes.net>
2023-10-15 19:25:32 +0200

    Adding data.

--- marey-train-schedule.js
+++ marey-train-schedule.js
@@ -8,12 +8,34 @@ var t79TS = {};
 
 function initSchedule() {
     t79TS.d3OutputElm = document.getElementById("d3vis");
-    createSchedule();
+    openData();
 }
 
-function createSchedule() {
+function openData() {
+    d3.csv("data/distance.csv").then(
+        (result) => { createSchedule(result); }
+    );
+}
+
+function createSchedule(data) {
+
+    if (data != null) {
+        t79TS.data = data;
+    }
+    else {
+        data = t79TS.data;
+    }
+
     width = t79TS.d3OutputElm.clientWidth * 2;
     height = Math.floor(width * 0.66);
+
+    console.log(data);
+    const maxDistance = d3.max(data, (d) => parseInt(d.distance));
+    console.log(maxDistance);
+
+    const y = d3.scaleLinear()
+        .range([0,maxDistance])
+        .domain([10,height - 20])
 
     var svg = d3.create("svg")
         .attr("width", width)
